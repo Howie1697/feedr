92319bd636ad93362ec3d2b93c904e56e3bd66d0
Howard Gilmore <Howardgg1997@gmail.com>
2021-01-24 18:40:35 -0800

    Feedr project completed.

--- index.js
+++ index.js
@@ -8,34 +8,53 @@ $xhr.done(function (data) {
     }
 
     console.log(data);
-    for(i=0; i < 4; i++){
-    let secOne = $(`#section${i}`)
-    let aTag = $(`#section${i}`).children("a")
-    //$("#sectionOne").css("border", "3px solid blue");
-    // 19 is using aTag so no need to have $ for future refrence aTag.attr("href","https://www.google.com");
-    aTag.attr("href", data[i].url);
-    let title1 = aTag.children("h3").text(data[i].title);
-    let summary1 = secOne.children("h6").text(data[i].summary)
-    let secImg1 = $(`#impression${i}`)
-    let imgTag = $("<img>")
-    imgTag.attr("src", data[i].imageUrl).css("height", "50px");
-    secImg1.append(imgTag)
-    console.log(secImg1)
-}});
+    for (i = 0; i < 4; i++) {
+        let secOne = $(`#section${i}`)
+        let aTag = $(`#section${i}`).children("a")
+        //$("#sectionOne").css("border", "3px solid blue");
+        // 19 is using aTag so no need to have $ for future refrence aTag.attr("href","https://www.google.com");
+        aTag.attr("href", data[i].url);
+        let title1 = aTag.children("h3").text(data[i].title);
+        let summary1 = secOne.children("h6").text(data[i].summary)
+        let secImg1 = $(`#impression${i}`)
+        let imgTag = $("<img>")
+        imgTag.attr("src", data[i].imageUrl).css("height", "50px");
+        secImg1.append(imgTag)
+        console.log(secImg1)
+    }
+});
 $xhr.fail(function (err) {
     console.log(err)
 });
-/*Grab section one and change the info(children) inside of it.
-let secOne = $("#sectionOne")
-let aTag = $("#sectionOne").children("a")
-$("#sectionOne").css("border", "3px solid blue");
-// 19 is using aTag so no need to have $ for future refrence aTag.attr("href","https://www.google.com");
-$("#sectionOne").children("a").attr("href", "https://www.google.com");
-let title1 = aTag.children("h3").text("Space Flight News");
-let summary1 = secOne.children("h6").text("While SpaceX remains focused on Starship flight testing as the dust settles from SN8’s launch debut, the company continues to make slow...")
-//
-let secImg1 = $(".impressions").first()
-let imgTag = $("<img>")
-imgTag.attr("src", "https://www.defencetalk.com/wp-content/uploads/2020/01/space-force-logo.jpg").css("height", "50px");
-secImg1.append(imgTag)
-console.log(secImg1)*/
+
+let getFood = () => {
+    console.log("clicked");
+    $(`#section${0}`).children("img").remove()
+    var $xhr = $.getJSON('https://foodish-api.herokuapp.com/api/');
+    $xhr.done(function (data) {
+        if ($xhr.status !== 200) {
+            return;
+        }
+
+        console.log(data);
+        for (i = 0; i < 4; i++) {
+            let secOne = $(`#section${i}`)
+            let aTag = $(`#section${i}`).children("a")
+            //aTag.attr("href", data[i].url);
+            let title1 = aTag.children("h3").text(" ");
+            let summary1 = secOne.children("h6").text(" ")
+            let articleLine = $('.article').css("borderBottom", "0px");
+            console.log(title1)
+            let secImg1 = $(`#impression${i}`).children("img").remove()
+        }
+        let secImg1 = $(`#section${0}`)
+        let imgTag = $("<img>")
+        console.log(data.image)
+        imgTag.attr("src", data.image).css("height", "500px");
+        secImg1.append(imgTag)
+    });
+    $xhr.fail(function (err) {
+        console.log(err)
+    });
+};
+let foodLink = $('#food').click(getFood)
